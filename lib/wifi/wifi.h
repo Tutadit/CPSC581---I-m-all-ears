3d2ade98c9d19830ed1135d5e5ec1b9578066d92
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-11-20 12:22:05 -0700

    shh

--- lib/wifi/wifi.h
+++ lib/wifi/wifi.h
@@ -3,8 +3,8 @@
 
 #include <ESP8266WiFi.h>
 
-#define SSID "TELUS5828-2.4G"
-#define PASSPHRASE "s5t3vkv5nq"
+#define SSID ""
+#define PASSPHRASE ""
 
 #define WIFI_CONNECTION_RETRY 500 // ms
 #define WIFI_INITIAL_DELAY 10     // ms
